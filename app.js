652c6f371ef92341f23477890eb21d15c40b88b5
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-12-30 16:19:19 +0700

    display messages using ejs

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
